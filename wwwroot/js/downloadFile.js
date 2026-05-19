6ff4bd33df936c38dfb76301a0cc28d2ce2db7cf
Andrew England <andrewmartynengland@gmail.com>
2026-05-19 20:03:02 +0100

    Add functionality to save button for the ability to save a file

---
+++ wwwroot/js/downloadFile.js
@@ -0,0 +1,11 @@
+﻿window.downloadFile = (fileName, content) => {
+    const blob = new Blob([content], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    a.click();
+
+    URL.revokeObjectURL(url);
+};
