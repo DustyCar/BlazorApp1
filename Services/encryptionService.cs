abdee862c8dd51fbbd3bedf87c5861e5886856e7
Andrew England <andrewmartynengland@gmail.com>
2026-05-22 13:40:08 +0100

    Encrypt and Decrypt methods moved to a new encryptionService file

---
+++ Services/encryptionService.cs
@@ -0,0 +1,62 @@
+﻿namespace BlazorApp1.Services
+{
+    public class encryptionService
+    {
+        public string Encrypt( string input )
+        {
+            string outputText = "";
+
+            foreach( char letter in input )
+            {
+                char encryptedLetter;
+
+                if( char.IsLetter( letter ) )
+                {
+                    if( letter > 'w' )
+                    {
+                        encryptedLetter = (char)(letter - 23);
+                    }
+                    else
+                    {
+                        encryptedLetter = (char)(letter + 3);
+                    }
+                }
+                else
+                {
+                    encryptedLetter = letter;
+                }
+
+                outputText += encryptedLetter;
+            }
+            return outputText;
+        }
+
+        public string Decrypt( string output )
+        {
+            string inputText = "";
+
+            foreach( char letter in output )
+            {
+                char encryptedLetter;
+                if( char.IsLetter( letter ) )
+                {
+                    if( letter < 'd' )
+                    {
+                        encryptedLetter = (char)(letter + 23);
+                    }
+                    else
+                    {
+                        encryptedLetter = (char)(letter - 3);
+                    }
+                }
+                else
+                {
+                    encryptedLetter = letter;
+                }
+                inputText += encryptedLetter;
+            }
+            return inputText;
+        }
+
+    }
+}
